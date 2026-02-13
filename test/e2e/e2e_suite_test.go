44eabacd000aee84a283f7cb2ed5a12e66b4bc5b
hammadzf <hammad.zafar@y7mail.com>
2026-02-13 17:49:30 +0100

    fix e2e tests

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -39,7 +39,7 @@ var (
 
 	// projectImage is the name of the image which will be build and loaded
 	// with the code source changes to be tested.
-	projectImage = "example.com/tf-state-rescuer:v0.0.1"
+	projectImage = "tf-rescuer:stable-1.0"
 )
 
 // TestE2E runs the end-to-end (e2e) test suite for the project. These tests execute in an isolated,
